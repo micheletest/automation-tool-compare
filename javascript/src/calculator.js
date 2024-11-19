68aec30eb9743169cfd882b840822686613c7998
Michele Martone <michele.martone@gmail.com>
2024-11-19 17:57:34 -0500

    implement the javascript version of the calculator

---
+++ javascript/src/calculator.js
@@ -0,0 +1,69 @@
+import fs from "fs";
+import path from "path";
+import papa from "papaparse";
+import { firefox } from "playwright";
+
+const CALCULATOR_URL = "https://treasurydirect.gov/BC/SBCPrice";
+
+const readCSVFile = async (filePath) => {
+  return new Promise((resolve, reject) => {
+    const fileStream = fs.createReadStream(filePath, "utf8");
+
+    const data = [];
+
+    papa.parse(fileStream, {
+      header: false,
+      skipEmptyLines: true,
+      step: (row) => {
+        data.push(row.data);
+      },
+      complete: () => {
+        resolve(data);
+      },
+      error: (error) => {
+        reject(error);
+      },
+    });
+  });
+};
+
+const calculateBond = async (page, bond) => {
+  await page.locator("[name='Series']").selectOption(bond[0]);
+  await page.locator("[name='Denomination']").selectOption(bond[1]);
+  await page.locator("[name='IssueDate']").fill(bond[2]);
+  await page.locator("[name='SerialNumber']").fill(bond[3]);
+  await page.locator("[value='CALCULATE']").click();
+};
+
+(async () => {
+  const args = process.argv.slice(2);
+  if (args.length < 1) {
+    console.error("Please provide the path to a CSV file.");
+    process.exit(1);
+  }
+
+  const csvFilePath = args[0];
+  const bonds = await readCSVFile(csvFilePath);
+  const browser = await firefox.launch({
+    headless: false,
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://treasurydirect.gov/BC/SBCPrice");
+
+  for (const bond of bonds) {
+    console.log(bond);
+    await calculateBond(page, bond);
+  }
+  await page.getByAltText("Save this list.").click();
+  const html = await page.content();
+  fs.writeFile("saved-bonds.html", html, "utf8", (err) => {
+    if (err) {
+      console.error(`Error writing to file:`, err);
+    } else {
+      console.log(`File written successfully to file`);
+    }
+  });
+  await context.close();
+  await browser.close();
+})();
